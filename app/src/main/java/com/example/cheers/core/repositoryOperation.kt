ef0f7907081fb4022cb6aff994e7246acedb580f
kushal <kushaldhengole@gmail.com>
2023-03-28 13:28:18 +0530

    Core

---
+++ app/src/main/java/com/example/cheers/core/repositoryOperation.kt
@@ -0,0 +1,4 @@
+package com.example.cheers.core
+
+object CommmonRepository {
+}
--- app/src/main/java/com/example/cheers/util/hideBottomSheet.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cheers.util
-
-interface hideBottomSheet {
-    fun showBottomSheet(hide:Boolean=true)
-}
